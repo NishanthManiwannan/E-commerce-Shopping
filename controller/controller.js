78bf13d2cb5b9965408fdf0a217063990c31d94e
nishanth <m.nishanth1397@gmail.com>
2022-02-23 18:21:00 +0530

    server - delete and update admin product

--- controller/controller.js
+++ controller/controller.js
@@ -1,23 +1,49 @@
-import mongoConnection from '../model/model.js'
+import mongoose from "mongoose";
+import mongoConnection from "../model/model.js";
 
 export const getProducts = async (req, res) => {
-    try{
-        const products = await mongoConnection.find();
-        console.log(products);
-        res.status(200).json(products);
-    }catch(err){
-        res.status(404).json({ message: err.message });
-    }
-}
+  try {
+    const products = await mongoConnection.find();
+    console.log(products);
+    res.status(200).json(products);
+  } catch (err) {
+    res.status(404).json({ message: err.message });
+  }
+};
 
 export const createNewProducts = async (req, res) => {
-    const products = req.body;
-    const newProducts = new mongoConnection(products);
+  const products = req.body;
+  const newProducts = new mongoConnection(products);
 
-    try{
-        await newProducts.save();
-        res.status(200).json(products);
-    }catch(err){
-        res.status(404).json({ message: err.message });
-    }
-}
+  try {
+    await newProducts.save();
+    res.status(200).json(products);
+  } catch (err) {
+    res.status(404).json({ message: err.message });
+  }
+};
+
+export const updateProduct = async (req, res) => {
+  const { id: _id } = req.params;
+  const product = req.body;
+
+  if (!mongoose.Types.ObjectId.isValid(_id))
+    return res.status(404).send("Id not found");
+
+  const updateProduct = await mongoConnection.findByIdAndUpdate(
+    _id,
+    { ...product, _id },
+    { new: true }
+  );
+
+  res.json(updateProduct);
+};
+
+export const deleteProduct = async (req, res) => {
+    const { id } = req.params;
+    if (!mongoose.Types.ObjectId.isValid(id))
+      return res.status(404).send("No product with that id");
+    await mongoConnection.findByIdAndRemove(id);
+  
+    res.json({ message: "Product deleted" });
+  };
--- routes/router.js
+++ routes/router.js
@@ -1,9 +1,11 @@
 import express from 'express';
-import { getProducts, createNewProducts } from '../controller/controller.js';
+import { getProducts, createNewProducts, updateProduct, deleteProduct } from '../controller/controller.js';
 
 const router = express.Router();
 
 router.get('/',getProducts);
 router.post('/', createNewProducts)
+router.patch('/:id', updateProduct)
+router.delete('/:id', deleteProduct)
 
 export default router;
